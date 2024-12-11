024882ba8a61538644bea437b65275c98e55350f
Saksham Kaushik <sakshamkauhsik3047@gmail.com>
2024-12-11 16:23:35 +0530

    test2

--- src/app/CardsCarousel/page.tsx
+++ src/app/CardsCarousel/page.tsx
@@ -13,7 +13,7 @@ export default function CardsCarousel() {
   return (
     <div className="w-full h-full py-20">
       <h2 className="max-w-7xl pl-4 mx-auto text-xl md:text-5xl font-bold text-neutral-800 dark:text-neutral-200 font-sans">
-        Get to know your iSad.
+        Get to know about 3d Spaces.
       </h2>
       <Carousel items={cards} />
     </div>
